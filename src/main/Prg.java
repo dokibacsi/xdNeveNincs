de693ff7c8ae551808521138d1d9c8569c3a0f7a
dokibacsi <kvalcsi02@gmail.com>
2024-09-30 13:24:22 +0200

    int fv() implementalas

--- src/main/Prg.java
+++ src/main/Prg.java
@@ -1,12 +1,15 @@
 package main;
+
 public class Prg {
+
     public static void main(String[] args) {
         Prg prg = new Prg();
         int i = prg.fv();
     }
 
     private int fv() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        System.out.println("Nem static fgv");
+        return 1;
     }
-    
+
 }
